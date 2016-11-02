684037fd3c8b3af3431ae8a4920c3d8c3b58476c
Sung Kook Kim <skkeem@gmail.com>
2016-11-02 13:18:27 +0900

    function environment

--- 1.scm
+++ 1.scm
@@ -1,17 +1,48 @@
 ; Environment
+;; implementation : assoc-list
 ;; env's key will be just symbol
+(define env.init '())
+
+(define (extend env variables values)
+  (cond ((pair? variables)
+         (if (pair? values)
+             (cons (cons (car variables) (car values))
+                   (extend env (cdr variables) (cdr values)))
+             (wront "Too less values")))
+        ((null? variables)
+         (if (null? values)
+             env
+             (wrong "Too much values")))
+        ((symbol? variables) (cons (cons variables values) env))))
+
 (define (lookup key env)
   ;;; symbol(key for variable) environment -> value
-  '())
-(define (update! key env e)
-  '())
+  (if (pair? env)
+      (if (eq? (caar env) key)
+          (cdar env)
+          (lookup key (cdr env)))
+      (wrong "No such binding" id))) ;; error
+
+(define (update! key env value)
+  ;;; -> value
+  (if (pair? env)
+      (if (eq? (caar env) key)
+          (begin (set-cdr! (car env) value)
+                 value) ;; value
+          (update! key (cdr env) value))
+      (wrong "No such  binding" key))) ;; error
 
 ; Function
-(define (make-function params body env)
+;; Implementation : function
+(define (make-function variables body env)
   ;;; list-of-params expression environment -> value
-  '())
-(define (invoke func args)
-  '())
+  (lambda (values)
+    (eprogn body (extend  variables values))))
+
+(define (invoke fn args)
+  (if (procedure? fn)
+      (fn args)
+      (wrong "Not a function" fn))) ;; error
 
 ; error
 (define (wrong msg e)
@@ -23,33 +54,33 @@
 (define empty-begin 813) ;; issue
 (define (eprogn exps env)
   (if (pair? exps)
-     (if (pair? (cdr exps))
-        (begin (evaluate (car exps) env)
-              (eprogn (cdr exps) env))
-        (evaluate (car exps) env)) ;; value
-     empty-begin)) ;; value for (begin)
+      (if (pair? (cdr exps))
+          (begin (evaluate (car exps) env)
+                 (eprogn (cdr exps) env))
+          (evaluate (car exps) env)) ;; value
+      empty-begin)) ;; value for (begin)
 (define (evlis exps env) ;; order of evaluation
   (if (pair? exps)
-     (let ((argument1 (evaluate (car exps) env)));;left to right
-       (cons argument1 (evlis (cdr exps) env)))
-     '()))
+      (let ((argument1 (evaluate (car exps) env)));;left to right
+        (cons argument1 (evlis (cdr exps) env)))
+      '()))
 ; eval
 (define (evalute e env)
   ;;; expression environment -> value
   (if (atom? e)
-     (cond
-       ((symbol? e) (lookup e env))
-       ((or (number? e)(string? e)(char? e)(boolean? e)(vector? e)) e) ;; autoquote
-       (else (wrong "Cannot evaluate" e))) ;; error?
-     (case (car e)
-       ;; special operator redefine meaningless.
-       ((quote)  (cadr e)) ;; (quote 1) ?
-       ;; haven't specified the value for boolean.
-       ((if)      (if (not (eq? (evalutae (cadr e) env) the-false-value))
-                     (evaluate (caddr e) env)
-                     (evaluate (cadddr e) env)))
-       ((begin)  (eprogn (cdr e) env)) ;; value for eprogn?
-       ((set!)    (update! (cadr e) env (evaluate (caddr e) env)))
-       ((lambda) (make-function (cadr e) (cddr e) env))
-       (else     (invoke (evaluate (car e) env)
-                        (evlis (cdr e) env))))))
+      (cond
+        ((symbol? e) (lookup e env))
+        ((or (number? e)(string? e)(char? e)(boolean? e)(vector? e)) e) ;; autoquote
+        (else (wrong "Cannot evaluate" e))) ;; error?
+      (case (car e)
+        ;; special operator redefine meaningless.
+        ((quote)  (cadr e)) ;; (quote 1) ?
+        ;; haven't specified the value for boolean.
+        ((if)     (if (not (eq? (evalutae (cadr e) env) the-false-value))
+                      (evaluate (caddr e) env)
+                      (evaluate (cadddr e) env)))
+        ((begin)  (eprogn (cdr e) env)) ;; value for eprogn?
+        ((set!)   (update! (cadr e) env (evaluate (caddr e) env))) 
+        ((lambda) (make-function (cadr e) (cddr e) env))
+        (else     (invoke (evaluate (car e) env)
+                          (evlis (cdr e) env))))))
